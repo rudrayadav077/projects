4e8bda03ced2e5725e35284e627d9d6c25aec4bd
Rudra yadav <rudray00707@gmail.com>
2024-06-21 13:22:35 +0530

    Add files via upload

---
+++ python/String/basicString.py
@@ -0,0 +1,10 @@
+a = "hello world"
+print(len(a))   #len() is a function which is used to count the length of the String 
+ 
+if "hello" in a: #HERE IN IS A KEYWORD WHICH IS USED TO SHOW LIKE IF THE STRING HELLO IS IN a THEN PRINT TURE 
+    print(True)
+
+     #THIS IS HOW WE CAN USE FOR LOOP IN STRING
+
+for i in range(0,len(a)):
+    print(a[i])
